03696ed3f862baeba719f59a71a3a452ac3e6aab
milospp <misa01523@gmail.com>
2021-03-18 16:00:03 +0100

    Initial frontend and backend structure

---
+++ backed/src/main/java/isa9/Farmacy/FarmacyApplication.java
@@ -0,0 +1,14 @@
+package isa9.Farmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacyApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Ajmo piti ajmo zagalamit!");
+		SpringApplication.run(FarmacyApplication.class, args);
+	}
+
+}
---
+++ backed/src/test/java/isa9/Farmacy/FarmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package isa9.Farmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
