1047370f690f5e476f8716356d4d9d0dcaf960c3
milospp <misa01523@gmail.com>
2021-04-04 20:30:52 +0200

    Added hibernate and lombook to maven

--- backend/src/main/java/isa9/Farmacy/model/User.java
+++ backend/src/main/java/isa9/Farmacy/model/User.java
@@ -1,18 +1,23 @@
 package isa9.Farmacy.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class User {
 
-    private Address address;
-    private String phoneNumber;
     private Long id;
     private String name;
     private String surname;
     private String email;
     private String password;
+    private Address address;
+    private String phoneNumber;
     private UserRole role;
-
-    public User() {
-    }
 
     @Deprecated
     public User(Long id, String name, String surname, String email, String password, Address address, String phoneNumber) {
@@ -25,93 +30,4 @@ public abstract class User {
         this.password = password;
     }
 
-    public User(Long id, String name, String surname, String email, String password, Address address, String phoneNumber, UserRole userRole) {
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-        this.role = userRole;
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return "User: " + name + " " + surname + " " + email + " " + password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
 }
