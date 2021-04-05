fa93e027bb2646b3eed355e248f1c04d3353fda4
milospp <misa01523@gmail.com>
2021-04-05 23:02:39 +0200

    Example for postgre data initialization

---
+++ backend/src/main/resources/data-postgres.sql
@@ -0,0 +1,3 @@
+insert into address (id, city, number, state, street) values (1, 'Citty', 'Numbeer', 'Sstate', 'Street')
+insert into users (id, address_id, email, name, password, phone_number, role, surname) values (1, 1, 'nesto@nesto.com', 'name', 'password', '012456789', 1, 'Surname')
+insert into patient (points, id) values (10, 1)
