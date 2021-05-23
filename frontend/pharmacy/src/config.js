db5a8b62e3bbfa5929f4f49b80eae8041d8796e2
milospp <misa01523@gmail.com>
2021-05-23 12:27:18 +0200

    Test frontend deploy 5

--- frontend/pharmacy/src/config.js
+++ frontend/pharmacy/src/config.js
@@ -1,5 +1,12 @@
-const config = {
+var config = {
     apiUrl: "https://localhost:8080/api"
 }
+
+if (window.location.hostname == "pharmacy-tim9.herokuapp.com"){
+    config = {
+        apiUrl: "https://pharmacy-tim9.herokuapp.com/api"
+    }
+}
+
 
 export default config;
