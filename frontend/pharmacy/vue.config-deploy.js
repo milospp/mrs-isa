d32930b1d464fddb89934c750c7e373dfd05ac9c
milospp <misa01523@gmail.com>
2021-05-23 02:00:36 +0200

    Test frontend /api redirect

--- frontend/pharmacy/vue.config-deploy.js
+++ frontend/pharmacy/vue.config-deploy.js
@@ -1,18 +1,12 @@
 const path = require('path');
 
-// module.exports = {
-//     devServer: {
-//         proxy: {
-//             '/api': {
-//                 target: 'http://localhost:8080'
-//             }
-//         }
-//     }
-// }
-
 module.exports = {
     devServer: {
-      proxy: 'https://pharmacy-tim9.herokuapp.com'
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080'
+            }
+        }
     }
   }
 
