a815db62a4a3efd224fa329d77f86feb5023c9d4
milospp <misa01523@gmail.com>
2021-05-23 13:22:38 +0200

    Added heroku front server to allowed crossorigins

--- frontend/pharmacy/src/config-local.js
+++ frontend/pharmacy/src/config-local.js
@@ -2,4 +2,6 @@ const config = {
     apiUrl: "https://localhost:8080/api"
 }
 
+console.log("config-local");
+
 export default config;
--- frontend/pharmacy/src/config.js
+++ frontend/pharmacy/src/config.js
@@ -2,11 +2,12 @@ var config = {
     apiUrl: "https://localhost:8080/api"
 }
 
-if (window.location.hostname == "pharmacy9.herokuapp.com"){
+if (window.location.hostname != "pharmacy9.herokuapp.com"){
     config = {
         apiUrl: "https://pharmacy-tim9.herokuapp.com/api"
     }
 }
 
+console.log("config");
 
 export default config;
