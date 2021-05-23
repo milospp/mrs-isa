7c6de57b64fa2d99bbc7d1b5ece7c42ff95e6a44
milospp <misa01523@gmail.com>
2021-05-23 02:07:11 +0200

    SPA attempt 2

--- frontend/pharmacy/server.js
+++ frontend/pharmacy/server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname + "/dist/index.html");
-});
+// app.use(express.static(__dirname + "/dist/"));
+// app.get(/.*/, function(req, res) {
+//     res.sendFile(__dirname + "/dist/index.html");
+// });
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/dist/'));
+
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/dist/index.html'));
+
+}
+
+
 app.listen(port);
 
 console.log("Server started...")
