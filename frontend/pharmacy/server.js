78040ef30bc908977cebc08083be6fe0cdddf188
milospp <misa01523@gmail.com>
2021-05-23 02:18:46 +0200

    SPA attempt 3

--- frontend/pharmacy/server.js
+++ frontend/pharmacy/server.js
@@ -7,6 +7,12 @@ const app = express();
 //     res.sendFile(__dirname + "/dist/index.html");
 // });
 
+var request = require('request');
+app.get('/api', function(req,res) {
+  //modify the url in any way you want
+  var newurl = 'http://localhost:8080';
+  request(newurl).pipe(res);
+});
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/dist/'));
