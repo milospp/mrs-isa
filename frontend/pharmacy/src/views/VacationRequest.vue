cb976f77dd93e6a002a870170839237761c37a07
isidora-stanic <isidora.stanic@outlook.com>
2021-05-28 21:13:24 +0200

    Frontend form for sending vacation request

--- frontend/pharmacy/src/views/VacationRequest.vue
+++ frontend/pharmacy/src/views/VacationRequest.vue
@@ -1,25 +1,84 @@
 <template>
   <NavBar/>
-  <h1 class="title">Vacation Request</h1>
-  <div class="containter">
+  <div class="container">
+    <h1 class="title">Vacation Request</h1>
+    <div v-if="jobs && doctorsRole === 'DERMATOLOGIST'" class="form-group">
+      <label for="pharmacy">Pharmacy</label>
+      <select class="form-control" v-model="pharmacy" name="pharmacy" id="pharmacy">
+        <option :key="j" v-for="j in jobs" :value="j.pharmacyId">{{j.pharmacyName}}</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="typeOfRest">Type</label>
+      <select class="form-control" v-model="vacation.type" name="type" id="typeOfRest">
+        <option value="REST">Vacation</option>
+        <option value="ABSENCE">Absence</option>
+      </select>
+    </div>
+    <div class="row">
+      <div class="form-group col">
+          <label for="start">Start date</label>
+          <input id="start" class="form-control" type="datetime-local" v-model="vacation.startDate"/>
+      </div>
+      <div class="form-group col">
+          <label for="end">End date</label>
+          <input id="end" class="form-control" type="datetime-local" v-model="vacation.endDate"/>
+      </div>
+    </div>
+    <div class="form-group">
+        <label for="reason">Why should I take a leave?</label>
+        <input class="form-control" placeholder="Reason" id="reason">
+    </div>
+    <div class="form-group">
+        <button class="form-control btn btn-success" id="send" @click="sendRequest()">Send Request</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import NavBar from '@/components/NavBar.vue'
+import AuthService from "@/service/AuthService.js";
+import PharmacyDataService from '@/service/PharmacyDataService.js';
 
 export default {
   name: 'PharmacistHomePage',
-  //props: ['role'],
   components: {
     NavBar
   },
   data() {
       return {
-          email: "test#test"
+          jobs: null,
+          vacation: {
+            type: 'REST',
+            startDate: new Date(),
+            endDate: new Date(),
+            reason: '',
+          }
       }
+  },
+  mounted() {
+    this.getJobs();
+  },
+  methods: {
+    sendRequest(){
+      alert('sending...');
+    },
+    getJobs(){
+      PharmacyDataService.getDoctorsJobs(this.doctor.id)
+      .then(response => {
+                    this.jobs = response.data;
+                    console.log("poslovi");
+                    console.log(response.data);
+                });
+    }
+  },
+  computed: {
+    doctor() {
+      return AuthService.getCurrentUser()
+    },
+    doctorsRole() {
+      return AuthService.getCurrentUser().role
+    }
   }
 }
 </script>
