eafe15ea637553773819ea88a9cba3168c5b67cc
dumitka <milica.djumic99@yahoo.com>
2021-06-02 22:03:19 +0200

    Sending mail to all suppliers when pharmacy admin accept some offer

--- backend/src/main/java/isa9/Farmacy/service/impl/db/dbOrderService.java
+++ backend/src/main/java/isa9/Farmacy/service/impl/db/dbOrderService.java
@@ -82,7 +82,6 @@ public class dbOrderService extends OrderServiceBase implements OrderService {
     public int chooseOffer(Offer ponuda, Long idOrder) {
         int povratna = -1;
         MedicineOrder narudzbenica = this.orderRepository.findById(idOrder).orElse(null);
-
         if (ponuda.getEndDate().isBefore(LocalDateTime.now())) return povratna;
         povratna = 0;
         narudzbenica.setChosenOffer(ponuda);
